fc33dba4041515ba55ef13909a46586a77bf6dc8
Aludey <aludey@yandex.ru>
2022-09-24 19:39:39 +0300

    Cleared App main class

--- src/main/java/com/aludey/sbertest/App.java
+++ src/main/java/com/aludey/sbertest/App.java
@@ -1,16 +1,6 @@
 package com.aludey.sbertest;
 
-import com.aludey.sbertest.products.cards.CreditCard;
-import com.aludey.sbertest.products.cards.DebitCard;
-import com.aludey.sbertest.utils.Currency;
-
-import java.math.BigDecimal;
-
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
